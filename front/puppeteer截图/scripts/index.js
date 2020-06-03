ddb6d8a52af29d215ae07f168786446cd0c31204
tianpanpan <tianpanpan@nobook.com>
2020-06-03 15:23:40 +0800

    添加通信等待加载完成在截图

--- front/puppeteer截图/scripts/index.js
+++ front/puppeteer截图/scripts/index.js
@@ -9,11 +9,20 @@ const puppeteer = require('puppeteer');
     dumpio: false
   });
   const page = await browser.newPage();
-  await page.goto('https://baidu.com');
+  await page.goto('http://192.168.1.214:8000/#/slideThumnail?optimize=true', {
+    waitUntil: "networkidle0",
+    load: true,
+    docontentloaded: true,
+  });
   await page.setViewport({
     width: 960,
     height: 540,
-});
+  });
+  
+  // 业务代码中设定window中的对象，存在表示加载完成
+  await page.waitForFunction('window.coverDataLoaded', {
+    polling: 1000
+  });
   await page.screenshot({ path: 'example.png' });
   await browser.close();
 })();
